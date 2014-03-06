754fdd1e53d3105969de5f2ba74bee115a9d4a2b
MaysWind <sunyunchen@gmail.com>
2014-03-07 01:34:35 +0800

    支持Where Having语句直接使用Linq

---
+++ DotMaysWind.Data/Linq/DeleteCommandExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq.Expressions;
+
+using DotMaysWind.Data.Command;
+
+namespace DotMaysWind.Data.Linq
+{
+    /// <summary>
+    /// 删除语句扩展类
+    /// </summary>
+    public static class DeleteCommandExtension
+    {
+        /// <summary>
+        /// 设置指定查询的语句并返回当前语句
+        /// </summary>
+        /// <param name="cmd">删除语句</param>
+        /// <param name="expr">Linq表达式</param>
+        /// <typeparam name="T">实体类类型</typeparam>
+        /// <exception cref="LinqNotSupportedException">Linq操作不支持</exception>
+        /// <returns>当前语句</returns>
+        public static DeleteCommand Where<T>(this DeleteCommand cmd, Expression<Func<T, Boolean>> expr)
+        {
+            return cmd.Where(SqlLinqCondition.Create<T>(expr));
+        }
+    }
+}
---
+++ DotMaysWind.Data/Linq/SelectCommandExtension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq.Expressions;
+
+using DotMaysWind.Data.Command;
+
+namespace DotMaysWind.Data.Linq
+{
+    /// <summary>
+    /// 选择语句扩展类
+    /// </summary>
+    public static class SelectCommandExtension
+    {
+        /// <summary>
+        /// 设置指定查询的语句并返回当前语句
+        /// </summary>
+        /// <param name="cmd">选择语句</param>
+        /// <param name="expr">Linq表达式</param>
+        /// <typeparam name="T">实体类类型</typeparam>
+        /// <exception cref="LinqNotSupportedException">Linq操作不支持</exception>
+        /// <returns>当前语句</returns>
+        public static SelectCommand Having<T>(this SelectCommand cmd, Expression<Func<T, Boolean>> expr)
+        {
+            return cmd.Having(SqlLinqCondition.Create<T>(expr));
+        }
+        /// <summary>
+        /// 设置指定查询的语句并返回当前语句
+        /// </summary>
+        /// <param name="cmd">选择语句</param>
+        /// <param name="expr">Linq表达式</param>
+        /// <typeparam name="T">实体类类型</typeparam>
+        /// <exception cref="LinqNotSupportedException">Linq操作不支持</exception>
+        /// <returns>当前语句</returns>
+        public static SelectCommand Where<T>(this SelectCommand cmd, Expression<Func<T, Boolean>> expr)
+        {
+            return cmd.Where(SqlLinqCondition.Create<T>(expr));
+        }
+    }
+}
