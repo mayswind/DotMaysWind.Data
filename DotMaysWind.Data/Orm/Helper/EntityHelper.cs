404a1c9816187f2b2ff1ffc31bc5340951d9119e
MaysWind <sunyunchen@gmail.com>
2014-03-08 01:53:30 +0800

    拆分创建Sql参数方法

--- DotMaysWind.Data/Orm/Helper/EntityHelper.cs
+++ DotMaysWind.Data/Orm/Helper/EntityHelper.cs
@@ -96,6 +96,44 @@ namespace DotMaysWind.Data.Orm.Helper
         }
 
         /// <summary>
+        /// 获取指定Sql参数
+        /// </summary>
+        /// <param name="attr">实体属性特性</param>
+        /// <param name="paramPrefix">参数名前缀</param>
+        /// <param name="value">属性值</param>
+        /// <returns>Sql参数集合</returns>
+        internal static SqlParameter InternalGetSqlParameter(DatabaseColumnAtrribute attr, String paramPrefix, Object value)
+        {
+            SqlParameter parameter = null;
+
+            if (attr.DbType.HasValue)
+            {
+                parameter = SqlParameter.Create(attr.ColumnName, paramPrefix + attr.ColumnName, attr.DbType.Value, value);
+            }
+            else
+            {
+                parameter = SqlParameter.Create(attr.ColumnName, paramPrefix + attr.ColumnName, value);
+            }
+
+            return parameter;
+        }
+
+        /// <summary>
+        /// 获取指定Sql参数
+        /// </summary>
+        /// <param name="attr">实体属性特性</param>
+        /// <param name="paramPrefix">参数名前缀</param>
+        /// <param name="dbType">数据类型</param>
+        /// <param name="value">属性值</param>
+        /// <returns>Sql参数集合</returns>
+        internal static SqlParameter InternalGetSqlParameter(DatabaseColumnAtrribute attr, String paramPrefix, DbType dbType, Object value)
+        {
+            SqlParameter parameter = SqlParameter.Create(attr.ColumnName, paramPrefix + attr.ColumnName, dbType, value);
+
+            return parameter;
+        }
+
+        /// <summary>
         /// 获取指定实体类的Sql参数集合
         /// </summary>
         /// <param name="entity">实体类</param>
@@ -116,28 +154,17 @@ namespace DotMaysWind.Data.Orm.Helper
             foreach (PropertyInfo prop in props)
             {
                 DatabaseColumnAtrribute attr = null;
-                SqlParameter parameter = null;
-
                 Object[] objs = prop.GetCustomAttributes(typeof(DatabaseColumnAtrribute), true);
 
                 foreach (Object obj in objs)
                 {
                     if ((attr = obj as DatabaseColumnAtrribute) != null)
                     {
+                        SqlParameter parameter = EntityHelper.InternalGetSqlParameter(attr, paramPrefix, prop.GetValue(entity, null));
+                        parameters.Add(parameter);
                         break;
                     }
                 }
-
-                if (attr.DbType.HasValue)
-                {
-                    parameter = SqlParameter.Create(attr.ColumnName, paramPrefix + attr.ColumnName, attr.DbType.Value, prop.GetValue(entity, null));
-                }
-                else
-                {
-                    parameter = SqlParameter.Create(attr.ColumnName, paramPrefix + attr.ColumnName, prop.GetValue(entity, null));
-                }
-
-                parameters.Add(parameter);
             }
 
             return parameters.ToArray();
