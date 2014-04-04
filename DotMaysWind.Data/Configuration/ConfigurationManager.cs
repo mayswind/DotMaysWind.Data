e25fb86486bd8253ce05cb5f51839184f7793d0e
MaysWind <sunyunchen@gmail.com>
2014-04-04 14:13:14 +0800

    修改为内部类和内部方法

--- DotMaysWind.Data/Configuration/ConfigurationManager.cs
+++ DotMaysWind.Data/Configuration/ConfigurationManager.cs
@@ -5,13 +5,13 @@ namespace DotMaysWind.Data.Configuration
     /// <summary>
     /// 数据库配置管理器
     /// </summary>
-    public static class ConfigurationManager
+    internal static class ConfigurationManager
     {
         /// <summary>
         /// 读取数据库配置
         /// </summary>
         /// <returns>数据库配置</returns>
-        public static DatabaseSettings GetDatabaseConfiguration()
+        internal static DatabaseSettings GetDatabaseConfiguration()
         {
             DatabaseSettings config = System.Configuration.ConfigurationManager.GetSection("dataConfiguration") as DatabaseSettings;
 
